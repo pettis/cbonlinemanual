c6e34bb3a3b80939a2300f2724cc0d0059b5382a
pettis <Juha Petjjrvi>
2013-07-04 19:57:06 +0300

    now checks if the page is published fixed main level navigation bug

--- manual/views.py
+++ manual/views.py
@@ -4,15 +4,15 @@ from django.http import Http404
 
 
 def index(request):
-    navigation = ManualPage.objects.filter(published=True)
+    navigation = ManualPage.objects.filter(published=True, parent=None)
     return render(request, 'view_page.html', {'navigation': navigation})
 
 
 def view_page(request, slug):
-    navigation = ManualPage.objects.filter(published=True)
+    navigation = ManualPage.objects.filter(published=True, parent=None)
     try:
-        page = ManualPage.objects.get(slug = slug)
+        page = ManualPage.objects.get(slug=slug, published=True)
     except ManualPage.DoesNotExist:
         raise Http404
     return render(request, 'view_page.html', {'navigation': navigation,
-                                              'page':page})
+                                              'page': page})
