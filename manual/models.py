425e01199d2bca0cbd68d7f17dbac225969b757d
pettis <Juha Petjjrvi>
2013-07-04 21:03:03 +0300

    manager to show only published objects

--- manual/models.py
+++ manual/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 
+class PublishedPagesManager(models.Manager):
+    def get_query_set(self):
+        return super(PublishedPagesManager, self).get_query_set().filter(published=True)
+
 class ManualPage(models.Model):
     parent = models.ForeignKey('ManualPage', null=True, blank=True)
     title = models.CharField(max_length=250)
@@ -8,6 +12,9 @@ class ManualPage(models.Model):
     modified = models.DateTimeField(auto_now=True)
     added = models.DateTimeField(auto_now_add=True)
     published = models.BooleanField(default=False)
+
+    all_objects = models.Manager()
+    objects = PublishedPagesManager()
 
     def __unicode__(self):
         return self.title
