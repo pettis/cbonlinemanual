7d5e6cc91dcc267ab5bd209dce6c0bf98bc7bc85
pettis <Juha Petjjrvi>
2013-07-04 19:12:57 +0300

    added slug field

--- manual/admin.py
+++ manual/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from .models import ManualPage
 
+
 class ManualPageAdmin(admin.ModelAdmin):
     class Meta:
         model = ManualPage
+        prepopulated_fields = {'slug': ('title',), }
+
 
 admin.site.register(ManualPage, ManualPageAdmin)
--- manual/models.py
+++ manual/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class ManualPage(models.Model):
     parent = models.ForeignKey('ManualPage', null=True, blank=True)
     title = models.CharField(max_length=250)
+    slug = models.SlugField()
     content = models.TextField()
     modified = models.DateTimeField(auto_now=True)
     added = models.DateTimeField(auto_now_add=True)
